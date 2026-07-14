554a15ea4b74c710c0290f2d739e8932241edd80
Aadarsh Kumar <kumaraadarsh1103@gmail.com>
2026-07-14 21:42:50 +0530

    Update File Organizer.py

--- File Organizer.py
+++ File Organizer.py
@@ -1,37 +1,25 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
-
 from organizer import organize_folder
 
-
-# Browse Folder
 def browse_folder():
     folder = filedialog.askdirectory()
-
     if folder:
         folder_path.set(folder)
 
-
-# Organize Files
 def organize_files():
-
     if folder_path.get() == "":
         messagebox.showwarning(
             "Warning",
             "Please select a folder first."
         )
         return
-
     try:
         moved, summary = organize_folder(folder_path.get())
-
         report = "Organization Completed!\n\n"
-
         for category, count in summary.items():
             report += f"{category}: {count}\n"
-
         report += f"\nTotal Files Moved: {moved}"
-
         messagebox.showinfo(
             "Success",
             report
@@ -42,30 +30,23 @@ def organize_files():
             "Error",
             str(e))
 
-
-# ---------------- GUI ---------------- #
-
 root = tk.Tk()
 root.title("File Organizer")
 root.geometry("600x220")
 root.resizable(False, False)
-
 title = tk.Label(
     root,
     text="Automatic File Organizer",
     font=("Arial", 18, "bold")
 )
 title.pack(pady=15)
-
 folder_path = tk.StringVar()
-
 entry = tk.Entry(
     root,
     textvariable=folder_path,
     width=60
 )
 entry.pack(pady=5)
-
 browse_btn = tk.Button(
     root,
     text="Browse Folder",
@@ -73,7 +54,6 @@ browse_btn = tk.Button(
     width=20
 )
 browse_btn.pack(pady=5)
-
 organize_btn = tk.Button(
     root,
     text="Organize Files",
@@ -83,5 +63,4 @@ organize_btn = tk.Button(
     fg="white"
 )
 organize_btn.pack(pady=15)
-
 root.mainloop()
